569dd29ab33193ee06cecb1fa11afebd8dfef3c8
RathodDeven <devenrathodrd@gmail.com>
2022-09-25 11:51:31 +0530

    upload modal working and returning cid

--- components/NewButton.js
+++ components/NewButton.js
@@ -1,27 +1,42 @@
 import React from 'react'
-import { AiOutlinePlus } from 'react-icons/ai'
+import { AiOutlinePlus, AiOutlineUpload } from 'react-icons/ai'
 import NewOptions from './NewOptions'
+import UploadFilesModal from './UploadFilesModal'
 import { modalType, usePopUpModal } from './wrapper/CustomPopUpProvider'
 
 const NewButton = () => {
     const { showModal } = usePopUpModal()
 
-    const showNewOptions = (e) => {
-        showModal({
-            component: <NewOptions />,
-            type: modalType.customposition,
-            onAction: () => {},
-            extraaInfo: {
-              top: e.currentTarget.getBoundingClientRect().top + 5 + 'px',
-              left: e.currentTarget.getBoundingClientRect().right + 20 +  'px'
-            }
-          })
+    // const showNewOptions = (e) => {
+    //     showModal({
+    //         component: <NewOptions />,
+    //         type: modalType.customposition,
+    //         onAction: () => {},
+    //         extraaInfo: {
+    //           top: e.currentTarget.getBoundingClientRect().top + 5 + 'px',
+    //           left: e.currentTarget.getBoundingClientRect().right + 20 +  'px'
+    //         }
+    //       })
+    // }
+    const onChangeFiles = (e) => {
+      const files = e.target.files
+      console.log(files)
+      showModal({
+        component: <UploadFilesModal initialFiles={files} />,
+        type: modalType.fullscreen,
+        onAction: () => {},
+        extraaInfo: {},
+        dontHide: true
+      })
     }
     return (
-    <button className='flex flex-row justify-center items-center bg-s-bg p-2 px-4 ml-4 mb-4 shadow-md rounded-full text-xl' onClick={showNewOptions}>
-        <AiOutlinePlus className='w-6 h-6 mr-1'/>
-        <div>New</div>
-    </button>
+      <>
+    <label htmlFor='upload-files' className='w-fit flex flex-row justify-center items-center bg-s-bg p-2 px-4 ml-4 mb-4 shadow-md rounded-full text-xl'>
+        <AiOutlineUpload className='w-6 h-6 mr-1'/>
+        <div>Upload</div>
+    </label>
+        <input type='file' id="upload-files" hidden multiple onChange={onChangeFiles}/>
+        </>
   )
 }
 
---
+++ components/UploadFilesModal.js
@@ -0,0 +1,97 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { AiOutlineClose } from 'react-icons/ai';
+import { Web3Storage } from 'web3.storage';
+import { usePopUpModal } from './wrapper/CustomPopUpProvider'
+
+const UploadFilesModal = ({initialFiles}) => {
+    const [status,setStatus] = useState('ready');
+    const { hideModal } = usePopUpModal()
+    const [files, setFiles] = useState(initialFiles)
+    const [totalFilesSize, setTotalFilesSize] = useState()
+    const [uploadedFilesSize, setUploadedFilesSize] = useState(0)
+
+    const convertSize = (size) => {
+        if(size < 1024) return size + ' B'
+        else if(size < 1024*1024) return (size/1024).toFixed(2) + ' KB'
+        else if(size < 1024*1024*1024) return (size/(1024*1024)).toFixed(2) + ' MB'
+        else return (size/(1024*1024*1024)).toFixed(2) + ' GB'
+    }
+
+    const getFilesSize = useCallback((files) => {
+        console.log(files)
+        return convertSize(Array.from(files).map(f => f.size).reduce((a, b) => a + b, 0))
+    },[])
+    useEffect(() => {
+        setTotalFilesSize(getFilesSize(files))
+    },[files,getFilesSize])
+
+    const handleUpload = async () => {
+        console.log('handleUpload')
+        setStatus('uploading')
+
+        const onRootCidReady = cid => {
+            console.log('uploading files with cid:', cid)
+        }
+         // when each chunk is stored, update the percentage complete and display
+        const totalSize = Array.from(files).map(f => f.size).reduce((a, b) => a + b, 0)
+        let uploaded = 0
+        const onStoredChunk = size => {
+        uploaded += size
+        
+        const pct = 100 * (uploaded / totalSize)
+        console.log(`Uploading... ${pct.toFixed(2)}% complete`)
+        console.log('uploaded', uploaded, 'of', totalSize, 'bytes')
+        console.log(`Uploading... ${uploaded} / ${totalFilesSize}`)
+
+        setUploadedFilesSize(convertSize(uploaded))
+        }
+    
+        // makeStorageClient returns an authorized Web3.Storage client instance
+        console.log('process.evn.ALCHEMY_ID',process.env.NEXT_PUBLIC_ALCHEMY_ID)
+        console.log('process.env.WEB3_STORAGE_TOKEN',process.env.NEXT_PUBLIC_WEB3_STORAGE_TOKEN)
+        const client = new Web3Storage({token: process.env.NEXT_PUBLIC_WEB3_STORAGE_TOKEN})
+    
+        // client.put will invoke our callbacks during the upload
+        // and return the root cid when the upload completes
+        const cid = await client.put(files, { onRootCidReady, onStoredChunk })
+        console.log('cid',cid)
+        setStatus('uploaded')
+    }
+   
+  return (
+    <div className='w-96 bg-s-bg p-4 rounded-xl'>
+        <div className='flex flex-row mb-4 bold font-bold justify-between w-full items-center'>
+            <div>
+                {status === 'ready' && `Upload Files (${totalFilesSize})`}
+                {status === 'uploading' && `Uploading Files (${uploadedFilesSize}/${totalFilesSize})`}
+                {status === 'uploaded' && `Uploaded Files (${totalFilesSize})`}
+            </div>
+            <AiOutlineClose className='w-6 h-6 cursor-pointer' onClick={hideModal}/>
+        </div>
+        <div className='flex flex-col mb-1'>
+            {files && Array.from(files).map((file,index) => {
+                return (
+                    <div key={index} className='bg-h-bg my-1 p-1 rounded-md flex flex-row justify-between items-center'>
+                        <div>{file.name + '  (' + convertSize(file.size) + ')'}</div>
+                        <AiOutlineClose className='w-4 h-4 cursor-pointer' onClick={() => {
+                            const newFiles = Array.from(files)
+                            newFiles.splice(index,1)
+                            setFiles(newFiles)
+                        }}/>
+
+                    </div>
+                )
+            })}
+        </div>
+        <div className='flex  w-full'>
+            <button onClick={handleUpload} disabled={status !== 'ready'} className='bg-p-btn p-1 rounded-md hover:bg-p-btn-hover'>
+                {status === 'ready' && 'Upload'}
+                {status === 'uploading' && 'Uploading'}
+                {status === 'uploaded' && 'Uploaded'}
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default UploadFilesModal
